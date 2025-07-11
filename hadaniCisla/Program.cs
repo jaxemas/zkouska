fa54baa02dae16d88d908ee728385ae6eb3d4270
jaxemas <standa.hadas1@gmail.com>
2025-07-11 09:25:07 +0200

    Add files via upload

---
+++ hadaniCisla/Program.cs
@@ -0,0 +1,28 @@
+﻿Random rnd = new Random ();
+int nahodneCislo = rnd.Next(1, 101);
+
+int i = 0;
+while ( i < 25 )
+
+{
+    Console.WriteLine("Napiste cislo od 1  do 100");
+    int cislo = int.Parse(Console.ReadLine());
+    i++;
+if ( cislo == nahodneCislo )
+{
+    Console.WriteLine($"mas to spravne, pouzil jsi {i} pokusu. ");
+    break;
+}
+else if (cislo > nahodneCislo )
+{
+    Console.WriteLine("cislo, ktere jste zadal je vetsi.");
+}
+else if (cislo< nahodneCislo )
+{
+    Console.WriteLine("cislo, ktere jste zadal je mensi.");
+}
+else
+{
+    Console.WriteLine("neco se pokazilo");
+}
+}

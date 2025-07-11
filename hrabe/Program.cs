f5b6d61e360338b19b35da7e0ca8033b42d2577b
jaxemas <standa.hadas1@gmail.com>
2025-07-11 09:18:19 +0200

    Add files via upload
    1.0

---
+++ hrabe/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("napiš číslo");
+int slovo0 =int.Parse(Console.ReadLine());
+
+Console.WriteLine("napiš číslo");
+int slovo1 =int.Parse(Console.ReadLine());
+
+Console.WriteLine("napiš číslo");
+int slovo2 =int.Parse(Console.ReadLine());
+Console.WriteLine($"{slovo0} {slovo1} {slovo2}");

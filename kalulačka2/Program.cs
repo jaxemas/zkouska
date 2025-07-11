3efbe28c0584d52a57b300e1feca6f65fa369643
jaxemas <standa.hadas1@gmail.com>
2025-07-11 09:26:56 +0200

    Add files via upload

---
+++ kalulačka2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+float a = 5f;
+float b = 6f;
+Console.WriteLine(a+b);
+Console.WriteLine(a-b);
+Console.WriteLine(a*b);
+Console.WriteLine(a/b);
+
+

8d73bc8891eefdef03270f7eeb9e2a482ae2d622
jaxemas <standa.hadas1@gmail.com>
2025-07-11 12:04:53 +0200

    Add files via upload

---
+++ ramecekNaTex/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Napiš text:");
+        string text = Console.ReadLine();
+
+        int width = text.Length + 4;  // šířka rámečku (text + okraje)
+
+        // horní hrana
+        Console.WriteLine("+" + new string('-', width - 2) + "+");
+
+        // řádek s textem (mezery + text + mezery)
+        Console.WriteLine("|  " + text + "  |");
+
+        // spodní hrana
+        Console.WriteLine("+" + new string('-', width - 2) + "+");
+    }
+}

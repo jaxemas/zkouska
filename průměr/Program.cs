fe64779f4ac80fdf6bad3533d1d99aa721f8d993
jaxemas <standa.hadas1@gmail.com>
2025-07-11 09:30:45 +0200

    Add files via upload

---
+++ průměr/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+float a = 5.6f;
+float b = 4.5f;
+float c = 3.8f;
+float d = (a+b+c)/3;
+Console.WriteLine(d);

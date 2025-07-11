ab396242e59aa352419b01d27757a68639ad9fb8
jaxemas <standa.hadas1@gmail.com>
2025-07-11 09:23:41 +0200

    Add files via upload

---
+++ dělitelnost/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("dobrý den dámy a pánové!");
+Console.WriteLine("napište číslo");
+int číslo = int.Parse(Console.ReadLine());
+if (číslo % 2 == 0) {
+    Console.WriteLine ("sudé");
+}
+else 
+{
+    Console.WriteLine ("liché");
+}

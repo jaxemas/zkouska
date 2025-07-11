ee4713a658175b2bb3389846102636eacc6ce1dc
jaxemas <standa.hadas1@gmail.com>
2025-07-11 09:32:11 +0200

    Add files via upload

---
+++ sčítání/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 0;
+Console.WriteLine(a);
+a += 10;
+Console.WriteLine(a);
+a *= 2;
+Console.WriteLine(a);
+a -= 5;
+Console.WriteLine(a);
